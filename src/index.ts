c71a98c5a52a6bf85f2c7255d1cbeb9d8f614bcf
Shahaf Shaked <shahaf@live.com>
2020-06-09 12:25:40 +0300

    assets management

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
 import { Config, TemplateOptions, MailOptions } from './types';
 
 const baseUrl = 'https://us-central1-muil-dev.cloudfunctions.net/v1';
@@ -11,12 +13,19 @@ export const init = (config: Config) => {
   projectId = config.projectId;
 };
 
+const ensureInitialized = () => {
+  if (!apiKey || !projectId) {
+    throw new Error('Service is not initialized (please init it by calling the init function)');
+  }
+};
+
 export const generate = async ({
   templateId,
   branch = 'master',
   props,
   type = 'html',
 }: TemplateOptions) => {
+  ensureInitialized();
   try {
     const { data } = await axios.post(
       `${baseUrl}/templates/${projectId}/${branch}/${templateId}?type=${type}`,
@@ -45,6 +54,7 @@ export const sendMail = async ({
   subject,
   attachments,
 }: TemplateOptions & MailOptions) => {
+  ensureInitialized();
   try {
     await axios.post(
       `${baseUrl}/templates/${projectId}/${branch}/${templateId}/email`,
@@ -63,6 +73,40 @@ export const sendMail = async ({
         },
       },
     );
+  } catch ({ response: { data } }) {
+    throw new Error(data.error?.description ?? data.error?.message ?? data);
+  }
+};
+
+export const uploadAsset = async (fileName: string, value: any) => {
+  ensureInitialized();
+  try {
+    const bodyData = new FormData();
+    bodyData.append('file', value, { filename: fileName });
+
+    const {
+      data: {
+        data: { url },
+      },
+    } = await axios.post(`${baseUrl}/assets/${projectId}/${fileName}`, bodyData, {
+      headers: { ...bodyData.getHeaders(), 'x-api-key': apiKey },
+    });
+
+    return url;
+  } catch (err) {
+    const {
+      response: { data },
+    } = err;
+    throw new Error(data.error?.description ?? data.error?.message ?? data);
+  }
+};
+
+export const deleteAsset = async (fileName: string) => {
+  ensureInitialized();
+  try {
+    await axios.delete(`${baseUrl}/assets/${projectId}/${fileName}`, {
+      headers: { 'x-api-key': apiKey },
+    });
   } catch ({ response: { data } }) {
     throw new Error(data.error?.description ?? data.error?.message ?? data);
   }
