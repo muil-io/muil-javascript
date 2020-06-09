c0dfc7f159c073b1e15b3aa34f2d47f6ca569732
Shahaf Shaked <shahaf@live.com>
2020-06-09 11:30:39 +0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,53 @@
 import axios from 'axios';
+import { Config, TemplateOptions, MailOptions } from './types';
+
+const baseUrl = 'https://us-central1-muil-dev.cloudfunctions.net/v1';
 
 let apiKey: string = null;
 let projectId: string = null;
 
-type Config = { apiKey: string; projectId: string };
 export const init = (config: Config) => {
   apiKey = config.apiKey;
   projectId = config.projectId;
 };
 
-type Attachment =
-  | { filename: string; type: string; templateId: string; props: object }
-  | { filename: string; content: string }
-  | { filename: string; path: string };
-
-type SendProps = {
-  from?: string;
-  to?: string;
-  cc?: string;
-  bcc?: string;
-  subject: string;
-  templateId: string;
-  branch?: string;
-  props: object;
-  attachments?: Attachment[];
+export const generate = async ({
+  templateId,
+  branch = 'master',
+  props,
+  type = 'html',
+}: TemplateOptions) => {
+  try {
+    const { data } = await axios.post(
+      `${baseUrl}/templates/${projectId}/${branch}/${templateId}?type=${type}`,
+      {
+        props,
+      },
+    );
+    return data;
+  } catch ({
+    response: {
+      data: { error },
+    },
+  }) {
+    throw new Error(error.description ?? error.message);
+  }
 };
-export const send = async ({
+
+export const sendMail = async ({
+  templateId,
+  branch = 'master',
+  props,
   from,
   to,
   cc,
   bcc,
   subject,
-  templateId,
-  branch = 'master',
-  props,
   attachments,
-}: SendProps) => {
+}: TemplateOptions & MailOptions) => {
   try {
     await axios.post(
-      `https://us-central1-muil-io.cloudfunctions.net/v1/templates/${projectId}/${branch}/${templateId}/email`,
+      `${baseUrl}/templates/${projectId}/${branch}/${templateId}/email`,
       {
         from,
         to,
@@ -54,7 +63,7 @@ export const send = async ({
         },
       },
     );
-  } catch (err) {
-    // empty block
+  } catch ({ response: { data } }) {
+    throw new Error(data.error?.description ?? data.error?.message ?? data);
   }
 };
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type Config = { apiKey: string; projectId: string };
+
+export type TemplateOptions = {
+  templateId: string;
+  branch?: string;
+  type?: 'html' | 'pdf' | 'png';
+  props: object;
+};
+
+export type MailOptions = {
+  from?: string;
+  to?: string | Array<string>;
+  cc?: string | Array<string>;
+  bcc?: string | Array<string>;
+  subject: string;
+  attachments?: Attachment[];
+};
+
+export type Attachment =
+  | ({ filename: string } & TemplateOptions)
+  | { filename: string; content: string }
+  | { filename: string; path: string };
